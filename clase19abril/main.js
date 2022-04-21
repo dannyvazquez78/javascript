537d3b40937ac635dcbc5d355df530121ca1a737
DanielVazquez <87391167+dannyvazquez78@users.noreply.github.com>
2022-04-21 15:09:39 -0600

    Add files via upload

---
+++ clase19abril/main.js
@@ -0,0 +1,119 @@
+// const generacion18 = [
+//     {
+//       signature: "Js",
+//       koders: ["Ivan", "Freddy"],
+//       mentor: "Ivan Diaz"
+//     },
+//     {
+//       signature: "HTML",
+//       koders: ["Jason", "Penny", "Freddy"],
+//       mentor: "Alan "
+//     },
+//     {
+//       signature: "CSS",
+//       koders: ["Arnold", "Gerald"],
+//       mentor: "David "
+//     },
+//     {
+//       signature: "Introduccion",
+//       koders: ["Steve", "Charlie"],
+//       mentor: "Elvira "
+//     },
+//     {
+//       signature: "React",
+//       koders: ["Ivan", "Harry", "Freddy"],
+//       mentor: "Alejandra"
+//     }
+//   ];
+
+//   const fitrarPorMateria = () => {
+
+//   }
+
+//   const saludar = (name) => console.log(`hola ${name}`);
+
+// saludar("ivan")
+
+
+
+// // for each
+
+// const numeros = [ 2, 4, 5]
+
+// numeros.forEach(( numero, index, arr) => console.log(numero * 2) )
+
+
+// Ejercicio 1
+// convertir los nombres en un Array
+// crear un cliclo para iterar variable de estudiantes
+// a estudiantes agregarle la palabra koder
+// agregar un numero 
+// unir los 3 elementos en un Array
+// imprimir los elementos
+
+
+const estudiantes = [
+  'Jason Vorhees',
+  'Penny Wise',
+  'Freddy Krueger'
+]
+
+
+   
+// function listado () {
+//     estudiantes.forEach ((nombre, index, arr)=> console.log(`koder ${index + 1}: ${nombre}`
+//     ))
+// }
+
+// listado()
+
+
+
+
+// output: 
+// [
+//   'koder 1: Jason Vorhees'
+//   'koder 2: Penny Wise',
+//   'koder 3: Freddy Krueger',
+// ]
+
+
+
+// Ejercicio 2
+
+// input:
+// [
+//   {
+//     koder: 'Arnold',
+//     score: 8,
+//   },
+//   {
+//     koder: 'Gerald',
+//     score: 8,
+//   },
+//   {
+//     koder: 'Gelga',
+//     score: 8,
+//   },
+//   {
+//     koder: 'Peer',
+//     score: 8,
+//   },
+//   {
+//     koder: 'Thomas',
+//     score: 8,
+//   }
+// ];
+
+
+// <60 F
+// <70 D
+// <80 C
+// <90 B
+// <100 A
+
+
+// output: {
+//   promedio del grupo: 90
+//   calificacion del grupo: A
+// }
