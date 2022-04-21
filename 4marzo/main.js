5bad96f09deb48119d17733c789827ce311b2410
DanielVazquez <87391167+dannyvazquez78@users.noreply.github.com>
2022-04-21 15:09:04 -0600

    Add files via upload

---
+++ 4marzo/main.js
@@ -0,0 +1,48 @@
+84938484
+"string"
+boleano = true or false
+
+{
+    nombre: "huesos"
+    raza: "galgo ingles"
+    color: "marron"
+    ladrar: function() {
+        return "gua guau"
+    }
+    olfatear: function(){
+        return "nif nif"
+    }
+}
+
+let perro = {
+    name: "huesos"
+    ladrar: function() {
+        console.log("guau");
+    }
+}
+
+let animal = "perro";
+animal = true;
+console.log (animal)
+
+const carro = {
+  marca: "ferrari",
+  color: "rojo",
+  encender: function() {
+    console.log("runnn")
+  }
+};
+carro.encender()
+
+function calcular() {
+  console.log("resultado es este")
+}
+
+calcular();
+
+function sumar(numero1, numero2) {
+    console.log(numero1 + numero2)
+    
+  }
+  
+  sumar(2, 4);
---
+++ 6abril/main.js
@@ -0,0 +1,31 @@
+let frutas = [ "manzana", "sandia", " melon"]
+
+let fruta = {
+    nombre: "limon",
+    color: "verde"
+}
+
+console.log (frutas.length)
+
+function sumar (num1, num2) {
+   
+    return num1 + num2;
+};
+let resultado = sumar (1,5);
+console.log(resultado)
+
+let nombre = "nestor";
+
+function persona(nombre){
+
+
+}
+
+let usuario = prompt("ingresa tu usuario")
+console.log(usuario, "usuario")
+
+if(10 > 8) {
+    console.log("es mayor");
+} else {
+    console.log ("es menor")
+}
