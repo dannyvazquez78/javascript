9ac495421fbe33dcf5a6acdaf045cb13b8afa32e
DanielVazquez <87391167+dannyvazquez78@users.noreply.github.com>
2022-04-29 11:02:58 -0600

    Add files via upload

---
+++ clase26Abril/main.js
@@ -0,0 +1,14 @@
+// const numbers = [1,5,9];
+
+// const resultadoReduce = numbers.reduce ((accum, item, index, arr) => {
+//     return accum + item;
+    
+
+// },0);
+
+// console.log(resultadoReduce);
+
+
+const estudiantes = [
+    
+]
